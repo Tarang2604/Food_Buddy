b025f4af1ec2f38210ce8d139c5df9b343b56015
Tarang Upadhyay <tarangupadhyay26@gmail.com>
2026-04-09 17:09:59 +0530

    commit changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
-const Contact =() =>{
-    return(
-        <div>
-            <h1 className="font-bold text-3xl p-4 m-4">Contact US Page </h1>
-            <form>
-       <input type="text" className="border border-black p-2 m-2" placeholder="name">
+// const Contact =() =>{
+//     return(
+//         <div>
+//             <h1 className="font-bold text-3xl p-4 m-4">Contact US Page </h1>
+//             <form>
+//        <input type="text" className="border border-black p-2 m-2" placeholder="name">
 
        </input  >
        <input className="border border-black p-2 m-2" placeholder="message">
